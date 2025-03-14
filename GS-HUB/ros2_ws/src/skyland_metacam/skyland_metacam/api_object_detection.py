33de98e77cf3720d97c4c27fd7a1755d8578a006
fangjiahuang <2637778334@qq.com>
2025-03-14 18:05:29 +0800

    断点测试

--- GS-HUB/ros2_ws/src/skyland_metacam/skyland_metacam/api_object_detection.py
+++ GS-HUB/ros2_ws/src/skyland_metacam/skyland_metacam/api_object_detection.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from skyland_metacam.service.object_detection_api import router as object_detection_router
 
+
 # 创建 FastAPI 应用
 app = FastAPI()
 
@@ -10,4 +11,4 @@ app.include_router(object_detection_router)
 # 启动服务时运行此文件
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=6060, log_level="debug")
